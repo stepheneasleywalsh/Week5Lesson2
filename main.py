6f8aee289a98e102cfb9c64e5984efa2bc39b6a9
stepheneasleywalsh <stephen.easley-walsh@studygroup.com>
2021-11-08 17:01:07 +0000

    Update

---
+++ main.py
@@ -0,0 +1,17 @@
+# THE BIO FUNCTION
+def bio(name,born,pronoun):
+    print("----------------------------------------")
+    age = 2021-born
+    future = born+67
+    print(name.capitalize(), "was born in", born)
+    print(pronoun.capitalize(), "will turn", age, "years old this year")
+    print(pronoun.capitalize(), "will be 67 in the year", future)
+    print("----------------------------------------")
+
+# PRINT BIOS
+bio("stephen",1984,"he")
+bio("mary",1990,"she")
+bio("jane",2000,"she")
+
+# EXIT
+quit()
